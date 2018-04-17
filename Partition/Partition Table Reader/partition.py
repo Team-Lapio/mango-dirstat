f31a8152e40764428c238cea116ec24601a76f0c
JaehunYoon <goodasd123@gmail.com>
2018-04-17 20:28:03 +0900

    Partition Table Reader Update

--- Partition/Partition Table Reader/partition.py
+++ Partition/Partition Table Reader/partition.py
@@ -1,5 +1,20 @@
 import struct
 
+
+def UnitChange(num):
+    sector_size = ['B', 'KB', 'MB', 'GB']
+
+    t = sizes = (num * 512.)
+
+    for i in range(4):
+        t = t / 1024
+        if int(t) > 0:
+            sizes = sizes / 1024
+        else:
+            break
+
+    return "%.2f %s" % (sizes, sector_size[i])
+
 def ExtendedPartition(part):
     print("ExtendedPartition")
 
@@ -9,9 +24,9 @@ def PrimaryPartition(part):  # 주 파티션 정보 출력
 
     # Starting LBA
     start = struct.unpack('<L', part[8:8+4])[0]
-    print("LBA value : {}".format(start))
+    print("LBA : {}".format(start))
 
     # Print Partition's size
     num = struct.unpack('<L', part[12:12+4])[0]
     size = (num * 512.) / 1024 / 1024 / 1024
-    print("Partition size : {} GB".format(size))
+    print("Partition size : {}".format(UnitChange(num)))
