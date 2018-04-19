41f764e734d49ffaf0d8bf2a07497f86817ac003
JaehunYoon <goodasd123@gmail.com>
2018-04-19 17:15:53 +0900

    Modified Partition code

--- Partition/Partition Table Reader/partition.py
+++ Partition/Partition Table Reader/partition.py
@@ -17,16 +17,11 @@ def UnitChange(num):
 
 def ExtendedPartition(part):
     print("ExtendedPartition")
+    start = struct.unpack('<L', part[8:8+4])[0]  # Starting LBA
+    print("[Extended] %10d" % (start))
 
 
 def PrimaryPartition(part):  # 주 파티션 정보 출력
-    print("PrimaryPartition")
-
-    # Starting LBA
-    start = struct.unpack('<L', part[8:8+4])[0]
-    print("LBA : {}".format(start))
-
-    # Print Partition's size
-    num = struct.unpack('<L', part[12:12+4])[0]
-    size = (num * 512.) / 1024 / 1024 / 1024
-    print("Partition size : {}".format(UnitChange(num)))
+    start = struct.unpack('<L', part[8:8+4])[0]  # Starting LBA
+    num = struct.unpack('<L', part[12:12+4])[0]  # Print Partition's size
+    print("[Primary] %10d %s" % (start, UnitChange(num)))
