0c629c669fe2304837cfcd66cb8a355ed5f24538
JaehunYoon <goodasd123@gmail.com>
2018-04-24 08:30:58 +0900

    mango-dirstat : added LogicalDrivePartition process

--- Partition/Partition Table Reader/partition.py
+++ Partition/Partition Table Reader/partition.py
@@ -1,5 +1,4 @@
 import struct
-from main import handle, base
 
 def UnitChange(num):
     sector_size = ['B', 'KB', 'MB', 'GB']
@@ -16,18 +15,33 @@ def UnitChange(num):
     return "%.2f %s" % (sizes, sector_size[i])
 
 def ExtendedPartition(part, start):
+    handle = open('\\\\.\\PhysicalDrive0', 'rb')  # test
+    base = struct.unpack('<L', part[8:8+4])[0]  # test
+
     print("ExtendedPartition")
-    print(base)
+    print(base)  # main.py의 base 값을 전달받을 방법을 알아봐야한다!!
     
     handle.seek((base + start) * 512)
     data = handle.read(0x200)
 
     if hex(data[510]) == '0x55' and hex(data[511]) == '0xaa':
         print("Partition Read Success")
+        
+        # 논리 드라이브 정보 획득
+        logical_part = []
+
+        for i in range(2):
+            logical_part.append(data[0x1BE + (i*0x10):0x1BE + (i*0x10) + 0x10])
+
+        if hex(logical_part[0][4]) != '0x0':  # 논리 드라이브 정보가 존재하면 조건문 실행
+            print("LogicalDrivePartition")
+        if hex(logical_part[1][4]) != '0x0':  # 확장 논리 드라이브 정보가 존재하면 조건문 실행
+            start = struct.unpack('<L', logical_part[1][8:8+4])[0]
+            ExtendedPartition(logical_part[1], start)
+
     
     # start = struct.unpack('<L', part[8:8+4])[0]  # Starting LBA
     # print("[Extended] %10d" % (start))
-    
 
 
 def PrimaryPartition(part):  # 주 파티션 정보 출력
